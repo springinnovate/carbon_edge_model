8e3a44dab2c673403aae26870441fff1534fccad
Rich Sharp <richpsharp@gmail.com>
2020-08-17 16:31:36 -0700

    missing a space

--- model_files.py
+++ model_files.py
@@ -87,7 +87,7 @@ def download_gs(base_uri, target_path, skip_if_target_exists=False):
     try:
         if not(skip_if_target_exists and os.path.exists(target_path)):
             subprocess.run(
-                f'/usr/local/gcloud-sdk/google-cloud-sdk/bin/gsutil cp'
+                f'/usr/local/gcloud-sdk/google-cloud-sdk/bin/gsutil cp '
                 f'{base_uri} {target_path}',
                 check=True, shell=True)
     except Exception:
