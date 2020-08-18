a7fff7d61b373168439477ec06435c1081c68ada
Rich Sharp <richpsharp@gmail.com>
2020-08-17 19:17:47 -0700

    no need to report everytime

--- model_fit_experiments.py
+++ model_fit_experiments.py
@@ -95,7 +95,8 @@ def generate_sample_points(
                     working_sample_list.append(nodata_replace)
                 else:
                     # nodata value, skip
-                    LOGGER.debug(f'got invalid value: {val} from {raster_path}')
+                    LOGGER.debug(
+                        f'got invalid value: {val} from {raster_path}')
                     valid_working_list = False
                     break
             if valid_working_list:
