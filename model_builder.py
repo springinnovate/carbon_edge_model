da5c42214178ac222f7809f85a9b73dde7aaab57
Rich Sharp <richpsharp@gmail.com>
2020-08-22 22:02:56 -0700

    debugging with logging

--- model_builder.py
+++ model_builder.py
@@ -135,6 +135,9 @@ def generate_sample_points_for_carbon_model(
             raster_index_to_array_list = []
             for index, (raster_path, band, nodata, nodata_replace,
                         gt, inv_gt) in enumerate(band_inv_gt_list):
+                if time.time() - last_time > 5.0:
+                    LOGGER.debug(f'working ... {points_remaining} left')
+                    last_time = time.time()
                 lng_min, lat_min, lng_max, lat_max = baccini_lng_lat_bb_list[
                     window_index]
                 x_min, y_min = [int(v) for v in (
