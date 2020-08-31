f1b02c8b91ae683a366b833f4161f50e40a49997
Rich Sharp <richpsharp@gmail.com>
2020-08-30 20:21:18 -0700

    param name list and mut info

--- feature_selection.py
+++ feature_selection.py
@@ -1,10 +1,9 @@
 """Feature selction preprocessor."""
-import pickle
-
 from sklearn.feature_selection import f_regression
 from sklearn.feature_selection import mutual_info_regression
 import numpy
 
+import carbon_model_data
 if __name__ == '__main__':
     X_vector_path = 'model_base_data/array_cache/X_array_1.npz'
     with open(X_vector_path, 'rb') as X_vector_file:
@@ -13,7 +12,15 @@ if __name__ == '__main__':
     with open(y_vector_path, 'rb') as y_vector_file:
         y_vector = numpy.load(y_vector_file)['arr_0']
 
+    parameter_name_list = [
+        val[0] for val in carbon_model_data.CARBON_EDGE_MODEL_DATA_NODATA] + [
+        f'{raster_type[0]}_gf_{dist}'
+        for raster_type in carbon_model_data.MASK_TYPES
+        for dist in carbon_model_data.MAX_EFFECT_EDGEDIST]
+
     f_reg = f_regression(X_vector, y_vector)
-    print(f_reg)
     mut_info = mutual_info_regression(X_vector, y_vector)
+    print(
+        '\n'.join([str((name, p_val, m_val)) for name, p_val, m_val in zip(
+            parameter_name_list, f_reg.pval, mut_info)]))
     print(mut_info)
