9c72700b77ea98992d8fbc5daa050327ea3cee46
Rich Sharp <richpsharp@gmail.com>
2020-08-30 17:28:29 -0700

    early stop if it doens't converg and give yousrelf lots of iterations

--- model_builder.py
+++ model_builder.py
@@ -77,6 +77,8 @@ MODEL_DICT = {
     'mlp_regressor': Pipeline([
         ('MLPRegressor', MLPRegressor(
             hidden_layer_sizes=(100,),
+            early_stopping=True,
+            max_iter=10000,
             activation='logistic'))
     ]),
 
