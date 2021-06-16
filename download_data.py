465b08542541e5f6f0a9b4521d83dbafce7cd429
Rich Sharp <richpsharp@gmail.com>
2021-06-15 19:56:51 -0700

    do a stack not an append

--- download_data.py
+++ download_data.py
@@ -553,7 +553,7 @@ def model_predict(
             if x_vector is None:
                 x_vector = numpy.array(array[valid_mask])
             else:
-                x_vector = numpy.stack(x_vector, array[valid_mask])
+                x_vector = numpy.stack((x_vector, array[valid_mask]), 1)
         y_vector = model(torch.from_numpy(x_vector))
         predicted_biomass_band.WriteArray(
             y_vector,
