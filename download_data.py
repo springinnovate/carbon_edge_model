210b9bf41eff524f6ade3505efc90cd46aa51f21
Rich Sharp <richpsharp@gmail.com>
2021-06-15 20:36:56 -0700

    do a reshape so it's 2d

--- download_data.py
+++ download_data.py
@@ -559,6 +559,7 @@ def model_predict(
                     f'x_vector.shape {x_vector.shape} vs {valid_array.shape}')
                 x_vector = numpy.append(x_vector, valid_array, axis=0)
         y_vector = model(torch.from_numpy(x_vector.T))
+        y_vector = y_vector.reshape(forest_array.shape)
         predicted_biomass_band.WriteArray(
             y_vector,
             xoff=offset_dict['xoff'],
