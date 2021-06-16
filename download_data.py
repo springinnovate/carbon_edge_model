128598b0529c6c8ec36a209e2500ce97e4831c52
Rich Sharp <richpsharp@gmail.com>
2021-06-15 19:11:21 -0700

    do a numpy 2d array

--- download_data.py
+++ download_data.py
@@ -551,7 +551,7 @@ def model_predict(
             continue
         for array in array_list:
             if x_vector is None:
-                x_vector = numpy.array(array[valid_mask])
+                x_vector = numpy.array([array[valid_mask]])
             else:
                 x_vector = numpy.append(x_vector, array[valid_mask], axis=1)
         y_vector = model(x_vector)
