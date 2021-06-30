0543b1f99a882a9a7d7e7264ff2913ccb5aa3cf4
Rich Sharp <richpsharp@gmail.com>
2021-06-30 13:19:56 -0700

    don't load optimizer state

--- train_model.py
+++ train_model.py
@@ -701,7 +701,7 @@ def train_cifar(
         model_path = f'model_{checkpoint_epoch}.dat'
         checkpoint = torch.load(model_path)
         model.load_state_dict(checkpoint['model_state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        #optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         last_epoch = checkpoint['epoch']
         loss = checkpoint['loss']
     else:
