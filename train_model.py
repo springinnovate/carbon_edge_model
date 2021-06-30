560c7e59fda846cb7fbda577c6d4268410501710
Rich Sharp <richpsharp@gmail.com>
2021-06-30 11:14:34 -0700

    is a dropoutlayer helpful?

--- train_model.py
+++ train_model.py
@@ -156,6 +156,7 @@ class NeuralNetwork(torch.nn.Module):
             torch.nn.ReLU(),
             torch.nn.Linear(l1, l1),
             torch.nn.ReLU(),
+            torch.nn.Dropout(p=0.1),
             torch.nn.Linear(l1, 1),
             torch.nn.ReLU(),
         )
