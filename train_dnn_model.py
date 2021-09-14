38aa8b2d9bb51d41cd2beff2b7da98df19f3cbd4
Rich Sharp <richpsharp@gmail.com>
2021-09-14 14:02:48 -0700

    wrapping up in a stable scheduler

--- train_dnn_model.py
+++ train_dnn_model.py
@@ -2,6 +2,7 @@
 from datetime import datetime
 import argparse
 import collections
+import functools
 import logging
 import math
 import multiprocessing
@@ -12,6 +13,9 @@ import time
 
 import matplotlib.pyplot as plt
 import pandas
+import ray
+from ray.tune.schedulers import ASHAScheduler
+from ray.tune import CLIReporter
 import sklearn.metrics
 
 logging.basicConfig(
@@ -47,12 +51,13 @@ torch.set_num_interop_threads(multiprocessing.cpu_count())
 BOUNDING_BOX = [-179, -60, 179, 60]
 
 WORKSPACE_DIR = 'workspace'
+FIG_DIR = os.path.join(WORKSPACE_DIR, 'fig_dir')
 ECOSHARD_DIR = os.path.join(WORKSPACE_DIR, 'ecoshard')
 ALIGN_DIR = os.path.join(WORKSPACE_DIR, f'align{"_".join([str(v) for v in BOUNDING_BOX])}')
 CHURN_DIR = os.path.join(WORKSPACE_DIR, f'churn{"_".join([str(v) for v in BOUNDING_BOX])}')
 CHECKPOINT_DIR = 'model_checkpoints'
 for dir_path in [
-        WORKSPACE_DIR, ECOSHARD_DIR, ALIGN_DIR, CHURN_DIR, CHECKPOINT_DIR]:
+        WORKSPACE_DIR, ECOSHARD_DIR, ALIGN_DIR, CHURN_DIR, CHECKPOINT_DIR, FIG_DIR]:
     os.makedirs(dir_path, exist_ok=True)
 RASTER_LOOKUP_PATH = os.path.join(WORKSPACE_DIR, f'raster_lookup{"_".join([str(v) for v in BOUNDING_BOX])}.dat')
 
@@ -156,15 +161,13 @@ PREDICTOR_LIST = [
 
 
 class NeuralNetwork(torch.nn.Module):
-    def __init__(self, M):
-        l1 = 100
+    def __init__(self, M, l1=100, l2=100):
         super(NeuralNetwork, self).__init__()
         self.flatten = torch.nn.Flatten()
         self.linear_relu_stack = torch.nn.Sequential(
             torch.nn.Linear(M, l1),
-            torch.nn.Linear(l1, l1),
-            torch.nn.Linear(l1, l1),
-            torch.nn.Linear(l1, 1),
+            torch.nn.Linear(l1, l2),
+            torch.nn.Linear(l2, 1),
             #torch.nn.Dropout(p=0.1),
         )
 
@@ -645,6 +648,199 @@ def r2_loss(x_val, y_val):
         LOGGER.exception('bad stuff in r2')
         return -100
 
+
+def load_data(geopandas_data, invalid_values, predictor_response_table):
+    """
+    Load and process data from geopandas data structure.
+
+    Args:
+        geopandas_data (str): path to geopandas file containing at least
+            the fields defined in the predictor response table and a
+            "holdback" field to indicate the test data.
+        invalid_values (list): list of (fieldname, value) tuples to
+            invalidate any fieldname entries that have that value.
+        predictor_response_table (str): path to a csv file containing
+            headers 'predictor' and 'response'. Any non-null values
+            underneath these headers are used for predictor and response
+            variables.
+
+    Return:
+        pytorch dataset tuple of (train, test) DataSets.
+    """
+    # load data
+    gdf = geopandas.read_file(geopandas_data)
+    for invalid_value_tuple in invalid_values:
+        key, value = invalid_value_tuple.split(',')
+        gdf = gdf[gdf[key] != float(value)]
+
+    # load predictor/response table
+    predictor_response_table = pandas.read_csv(predictor_response_table)
+    dataset_map = {}
+    for train_holdback_type, train_holdback_val in [
+            ('holdback', True), ('train', False)]:
+        predictor_response_map = collections.defaultdict(list)
+        gdf_filtered = gdf[gdf['holdback']==train_holdback_val]
+        for parameter_type in ['predictor', 'response']:
+            for parameter_id in predictor_response_table[parameter_type]:
+                if isinstance(parameter_id, str):
+                    if parameter_id == 'geometry.x':
+                        predictor_response_map[parameter_type].append(
+                            gdf_filtered['geometry'].x)
+                    elif parameter_id == 'geometry.y':
+                        predictor_response_map[parameter_type].append(
+                            gdf_filtered['geometry'].y)
+                    else:
+                        predictor_response_map[parameter_type].append(
+                            gdf_filtered[parameter_id])
+
+        x_tensor = torch.from_numpy(numpy.array(
+            predictor_response_map['predictor'], dtype=numpy.float32).T)
+        y_tensor = torch.from_numpy(numpy.array(
+            predictor_response_map['response'], dtype=numpy.float32).T)
+        dataset_map[train_holdback_type] = torch.utils.data.TensorDataset(
+            x_tensor, y_tensor)
+    return predictor_response_table['predictor'].count(), dataset_map['train'], dataset_map['holdback']
+
+
+def train_cifar_ray(
+        config, n_predictors, trainset, testset, n_epochs, working_dir,
+        checkpoint_dir=None, ):
+    model = NeuralNetwork(n_predictors, config["l1"], config["l2"])
+    device = 'cpu'
+    model.to(device)
+
+    loss_fn = torch.nn.MSELoss(reduction='mean')
+    optimizer = torch.optim.AdamW(
+        model.parameters(), lr=config["lr"])
+
+    if checkpoint_dir:
+        model_state, optimizer_state = torch.load(
+            os.path.join(checkpoint_dir, "checkpoint"))
+        model.load_state_dict(model_state)
+        optimizer.load_state_dict(optimizer_state)
+
+    test_abs = int(len(trainset) * 0.8)
+    train_subset, val_subset = torch.utils.data.random_split(
+        trainset, [test_abs, len(trainset) - test_abs])
+
+    train_loader = torch.utils.data.DataLoader(
+        train_subset,
+        batch_size=int(config["batch_size"]),
+        shuffle=True)
+    val_loader = torch.utils.data.DataLoader(
+        val_subset,
+        batch_size=int(config["batch_size"]),
+        shuffle=True)
+
+    last_epoch = 0
+    n_train_samples = len(train_loader)
+
+    training_loss_list = []
+    validation_loss_list = []
+
+    for epoch in range(n_epochs):  # loop over the dataset multiple times
+        training_running_loss = 0.0
+        epoch_steps = 0
+
+        for i, (predictor_t, response_t) in enumerate(train_loader):
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = model(predictor_t)
+            training_loss = loss_fn(outputs, response_t)
+            training_loss.backward()
+            optimizer.step()
+
+            # print statistics
+            training_running_loss += training_loss.item()
+
+            epoch_steps += 1
+            if i % 100 == 0:
+                LOGGER.debug(f'[{epoch+1+last_epoch}/{n_epochs+last_epoch}, {i+1}/{int(numpy.ceil(n_train_samples/config["batch_size"]))}]')
+
+        training_loss_list.append(training_running_loss/epoch_steps)
+
+        # Validation loss
+        val_loss = 0.0
+        val_steps = 0
+        for i, (predictor_t, response_t) in enumerate(val_loader):
+            with torch.no_grad():
+                outputs = model(predictor_t)
+                loss = loss_fn(outputs, response_t)
+                val_loss += loss.cpu().numpy()
+                val_steps += 1
+
+        validation_loss_list.append(val_loss/val_steps)
+
+        with ray.tune.checkpoint_dir(epoch) as checkpoint_dir:
+            path = os.path.join(checkpoint_dir, "checkpoint")
+            torch.save((model.state_dict(), optimizer.state_dict()), path)
+
+        outputs = model(trainset[:][0])
+        ray.tune.report(
+            loss=(val_loss / val_steps),
+            r2=sklearn.metrics.r2_score(outputs.detach(), trainset[:][1]))
+        #ray.tune.report(r2=sklearn.metrics.r2_score(outputs.detach(), trainset[:][1]))
+
+        figure_prefix = os.path.join(
+            working_dir, 'figdir',
+            f'{config["l1"]}_{config["l2"]}_{config["lr"]}')
+
+        expected_values = trainset[:][1].numpy().flatten()
+        actual_values = outputs.detach().numpy().flatten()
+
+        plot_metrics(
+            figure_prefix,
+            expected_values, actual_values, training_loss_list,
+            validation_loss_list)
+    print("Finished Training")
+
+
+def plot_metrics(
+        figure_prefix,
+        expected_values, actual_values, training_loss_list,
+        validation_loss_list):
+    # plot model correlation graph
+    fig, ax = plt.subplots(figsize=(12, 10))
+
+    # equivalent but more general
+    ax1 = plt.subplot(2, 1, 1)
+
+    # add a subplot with no frame
+    ax2 = plt.subplot(2, 1, 2, frameon=False)
+
+    ax1.scatter(expected_values, actual_values, c='b', s=0.25)
+    z = numpy.polyfit(expected_values, actual_values, 1)
+    trendline_func = numpy.poly1d(z)
+
+    r2 = sklearn.metrics.r2_score(actual_values, expected_values)
+
+    ax1.set_xlabel('expected values')
+    ax1.set_ylabel('actual values')
+    ax1.set_ylim(-100, 200)
+    ax1.plot(
+        expected_values,
+        trendline_func(expected_values),
+        "r--", linewidth=1.5)
+    ax1.set_title(f'Model Trained with lat/lng coordinates only $R^2={r2:.3f}$')
+
+    ax2.set_xlabel('epoch values')
+    ax2.set_ylabel('loss')
+    ax2.plot(
+        range(len(training_loss_list)),
+        training_loss_list,
+        "b-", linewidth=1.5, label='training loss')
+    ax2.plot(
+        range(len(validation_loss_list)),
+        validation_loss_list,
+        "r-", linewidth=1.5, label='validation loss')
+    ax2.legend()
+    ax2.set_title(f'Loss Function Model Trained with lat/lng coordinates')
+    plt.savefig(f'{figure_prefix}_model_loss_{len(validation_loss_list):02d}.png')
+    plt.close()
+
+
 def train_cifar(
         model, loss_fn, optimizer, ds_train, ds_holdback, n_epochs,
         batch_size, checkpoint_epoch=None):
@@ -664,7 +860,6 @@ def train_cifar(
     val_loader = torch.utils.data.DataLoader(
         ds_holdback, batch_size=batch_size, shuffle=True)
 
-    n_train_samples = len(ds_train)
     n_validation_samples = len(val_loader)
     print(f'{n_train_samples} samples to train {n_validation_samples} samples to validate')
 
@@ -824,53 +1019,50 @@ def main():
 
     parser.add_argument('--invalid_values', type=str, nargs='*', help=(
         'values to mask out of dataframe write as fieldname,value pairs'))
+    parser.add_argument(
+        '--num_samples', type=int, default=10,
+        help='number of times to do a sample to see best structure')
     args = parser.parse_args()
 
-    # load data
-    gdf = geopandas.read_file(args.geopandas_data)
-    for invalid_value_tuple in args.invalid_values:
-        key, value = invalid_value_tuple.split(',')
-        gdf = gdf[gdf[key] != float(value)]
+    n_predictors, trainset, testset = load_data(
+        args.geopandas_data, args.invalid_values,
+        args.predictor_response_table)
 
-    # load predictor/response table
-    predictor_response_table = pandas.read_csv(args.predictor_response_table)
-    dataset_map = {}
-    for train_holdback_type, train_holdback_val in [
-            ('holdback', True), ('train', False)]:
-        predictor_response_map = collections.defaultdict(list)
-        gdf_filtered = gdf[gdf['holdback']==train_holdback_val]
-        for parameter_type in ['predictor', 'response']:
-            for parameter_id in predictor_response_table[parameter_type]:
-                if isinstance(parameter_id, str):
-                    if parameter_id == 'geometry.x':
-                        predictor_response_map[parameter_type].append(
-                            gdf_filtered['geometry'].x)
-                    elif parameter_id == 'geometry.y':
-                        predictor_response_map[parameter_type].append(
-                            gdf_filtered['geometry'].y)
-                    else:
-                        predictor_response_map[parameter_type].append(
-                            gdf_filtered[parameter_id])
+    config = {
+        #"l1": ray.tune.sample_from(lambda _: 2 ** numpy.random.randint(2, 9)),
+        #"l2": ray.tune.sample_from(lambda _: 2 ** numpy.random.randint(2, 9)),
+        "l1": ray.tune.choice([100, 50, 25, 10]),
+        "l2": ray.tune.choice([100, 50, 25, 10]),
+        "lr": ray.tune.loguniform(args.learning_rate, args.learning_rate*1e3),
+        "batch_size": ray.tune.choice([1000, 500, 250, 100])
+    }
+    scheduler = ASHAScheduler(
+        metric="loss",
+        mode="min",
+        max_t=60, #args.n_epochs,
+        grace_period=10,
+        reduction_factor=2)
+    reporter = CLIReporter(
+        # parameter_columns=["l1", "l2", "lr", "batch_size"],
+        metric_columns=["loss", "accuracy", "training_iteration", "r2"])
 
-        x_tensor = torch.from_numpy(numpy.array(
-            predictor_response_map['predictor'], dtype=numpy.float32).T)
-        y_tensor = torch.from_numpy(numpy.array(
-            predictor_response_map['response'], dtype=numpy.float32).T)
-        dataset_map[train_holdback_type] = torch.utils.data.TensorDataset(
-            x_tensor, y_tensor)
+    result = ray.tune.run(
+        functools.partial(
+            train_cifar_ray, trainset=trainset, testset=testset,
+            n_epochs=args.n_epochs, n_predictors=n_predictors,
+            working_dir=os.getcwd()),
+        resources_per_trial={"cpu": 2},
+        config=config,
+        num_samples=args.num_samples,
+        scheduler=scheduler,
+        progress_reporter=reporter)
 
-    n_predictors = predictor_response_table['predictor'].count()
-    print(n_predictors)
-    model = NeuralNetwork(n_predictors)
-    #loss_fn = torch.nn.L1Loss(reduction='mean')
-    loss_fn = torch.nn.MSELoss(reduction='mean')
-    #loss_fn = lambda x, y: abs(1-r2_loss(x, y))
-    optimizer = torch.optim.AdamW(
-        model.parameters(), lr=args.learning_rate)
-    train_cifar(
-        model, loss_fn, optimizer, dataset_map['train'],
-        dataset_map['holdback'], args.n_epochs, args.batch_size,
-        checkpoint_epoch=args.last_epoch)
+    best_trial = result.get_best_trial("loss", "min", "last")
+    print("Best trial config: {}".format(best_trial.config))
+    print("Best trial final validation loss: {}".format(
+        best_trial.last_result["loss"]))
+    print("Best trial final validation R^2: {}".format(
+        best_trial.last_result["r2"]))
 
     LOGGER.debug('all done')
 
