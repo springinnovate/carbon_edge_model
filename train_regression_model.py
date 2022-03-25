bd752c83b03792989138725b8c6679331a2eeef8
Rich Sharp <rich@therealspring.org>
2022-03-25 10:25:54 -0700

    closing all the parens

--- train_regression_model.py
+++ train_regression_model.py
@@ -332,13 +332,14 @@ def main():
             # ('LinearSVR', make_pipeline(spline_features, StandardScaler(), PCA(n_components=n_components), LinearSVR(max_iter=max_iter, loss='squared_epsilon_insensitive', epsilon=1e-3, dual=False))),
             # ('LassoLarsCV', make_pipeline(spline_features, StandardScaler(), PCA(n_components=n_components), linear_model.LassoLarsCV(max_iter=max_iter, cv=10, eps=1e-3, normalize=False))),
             # ('LassoLars', make_pipeline(spline_features, StandardScaler(), PCA(n_components=n_components), linear_model.LassoLars(alpha=.1, normalize=False, max_iter=max_iter, eps=1e-3))),
-            ('LinearSVR', make_pipeline(poly_features, StandardScaler(), PCA(whiten=True), TransformedTargetRegressor(regressor=LinearSVR(max_iter=max_iter, loss='squared_epsilon_insensitive', epsilon=1e-3, dual=False), func=numpy.log, inverse_func=numpy.exp),
-            ('LassoLarsCV', make_pipeline(poly_features, StandardScaler(), PCA(whiten=True), TransformedTargetRegressor(regressor=LassoLarsCV(max_iter=max_iter, cv=10, eps=1e-3, normalize=False), func=numpy.log, inverse_func=numpy.exp),
-            ('LassoLars', make_pipeline(poly_features, StandardScaler(), PCA(whiten=True), TransformedTargetRegressor(regressor=LassoLars(alpha=.1, normalize=False, max_iter=max_iter, eps=1e-3), func=numpy.log, inverse_func=numpy.exp),
+            ('LinearSVR', make_pipeline(poly_features, StandardScaler(), PCA(whiten=True), TransformedTargetRegressor(
+                regressor=LinearSVR(max_iter=max_iter, loss='squared_epsilon_insensitive', epsilon=1e-3, dual=False), func=numpy.log, inverse_func=numpy.exp))),
+            ('LassoLarsCV', make_pipeline(poly_features, StandardScaler(), PCA(whiten=True), TransformedTargetRegressor(
+                regressor=LassoLarsCV(max_iter=max_iter, cv=10, eps=1e-3, normalize=False), func=numpy.log, inverse_func=numpy.exp))),
+            ('LassoLars', make_pipeline(poly_features, StandardScaler(), PCA(whiten=True), TransformedTargetRegressor(
+                regressor=LassoLars(alpha=.1, normalize=False, max_iter=max_iter, eps=1e-3), func=numpy.log, inverse_func=numpy.exp))),
             ]:
 
-TransformedTargetRegressor(regressor=LinearRegression(),
-...                                 func=np.log, inverse_func=np.exp)
 
         LOGGER.info(f'fitting data with {name}')
         model = reg.fit(trainset[0], trainset[1])
