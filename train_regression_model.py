ad6c45c8984614bd19197c872e3ff143e29926c1
Rich Sharp <rich@therealspring.org>
2022-03-25 09:42:16 -0700

    whiten

--- train_regression_model.py
+++ train_regression_model.py
@@ -331,9 +331,9 @@ def main():
             # ('LinearSVR', make_pipeline(spline_features, StandardScaler(), PCA(n_components=n_components), LinearSVR(max_iter=max_iter, loss='squared_epsilon_insensitive', epsilon=1e-3, dual=False))),
             # ('LassoLarsCV', make_pipeline(spline_features, StandardScaler(), PCA(n_components=n_components), linear_model.LassoLarsCV(max_iter=max_iter, cv=10, eps=1e-3, normalize=False))),
             # ('LassoLars', make_pipeline(spline_features, StandardScaler(), PCA(n_components=n_components), linear_model.LassoLars(alpha=.1, normalize=False, max_iter=max_iter, eps=1e-3))),
-            ('LinearSVR', make_pipeline(poly_features, StandardScaler(), PCA(), LinearSVR(max_iter=max_iter, loss='squared_epsilon_insensitive', epsilon=1e-3, dual=False))),
-            ('LassoLarsCV', make_pipeline(poly_features, StandardScaler(), PCA(), linear_model.LassoLarsCV(max_iter=max_iter, cv=10, eps=1e-3, normalize=False))),
-            ('LassoLars', make_pipeline(poly_features, StandardScaler(), PCA(), linear_model.LassoLars(alpha=.1, normalize=False, max_iter=max_iter, eps=1e-3))),
+            ('LinearSVR', make_pipeline(poly_features, StandardScaler(), PCA(whiten=True), LinearSVR(max_iter=max_iter, loss='squared_epsilon_insensitive', epsilon=1e-3, dual=False))),
+            ('LassoLarsCV', make_pipeline(poly_features, StandardScaler(), PCA(whiten=True), linear_model.LassoLarsCV(max_iter=max_iter, cv=10, eps=1e-3, normalize=False))),
+            ('LassoLars', make_pipeline(poly_features, StandardScaler(), PCA(whiten=True), linear_model.LassoLars(alpha=.1, normalize=False, max_iter=max_iter, eps=1e-3))),
             ]:
         LOGGER.info(f'fitting data with {name}')
         model = reg.fit(trainset[0], trainset[1])
