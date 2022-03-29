4f0521e25a0c859be0fcae58c6b7b3a7206ecde7
Rich Sharp <rich@therealspring.org>
2022-03-29 11:49:06 -0700

    name it different

--- train_regression_model.py
+++ train_regression_model.py
@@ -364,7 +364,7 @@ def main():
             POLY_ORDER, interaction_only=False, include_bias=False)
 
     for name, reg in [
-            ('LinearSVR', make_pipeline(poly_features, StandardScaler(), LinearSVR(max_iter=max_iter, loss='squared_epsilon_insensitive', epsilon=1e-3, dual=False))),
+            ('LinearSVR_v2', make_pipeline(poly_features, StandardScaler(), LinearSVR(max_iter=max_iter, loss='squared_epsilon_insensitive', epsilon=1e-3, dual=False))),
             #('LassoLarsCV', make_pipeline(poly_features, StandardScaler(),  LassoLarsCV(max_iter=max_iter, cv=10, eps=1e-3, normalize=False))),
             #('LassoLars', make_pipeline(poly_features, StandardScaler(),  LassoLars(alpha=.1, normalize=False, max_iter=max_iter, eps=1e-3))),
             ]:
