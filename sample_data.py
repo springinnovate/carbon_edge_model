2b49324c1639be4b8d78abd11c94e3b8256a6452
Rich Sharp <rich@therealspring.org>
2022-06-24 15:14:27 -0700

    holdback is false?

--- sample_data.py
+++ sample_data.py
@@ -317,7 +317,7 @@ def main():
         raster_path_set, args.sample_vector_path, target_box_wgs84,
         holdback_boxes, args.holdback_margin, args.n_samples,
         args.iso_names)
-    LOGGER.info(filtered_gdf_points.columns)
+    LOGGER.info(filtered_gdf_points['holdback'] is False)
 
     fig, ax = plt.subplots(figsize=(12, 10))
     ax.set_title(f'{len(filtered_gdf_points)} points')
