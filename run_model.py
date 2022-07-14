93003cd24c81b27354557f0f47df75cf3f556de2
Rich Sharp <rich@therealspring.org>
2022-07-14 16:21:55 -0700

    try override

--- run_model.py
+++ run_model.py
@@ -72,6 +72,7 @@ def main():
 
     LOGGER.info('clip input rasters to forest cover')
     aligned_predictor_path_list = []
+    gf_index = None
     for predictor_id, predictor_path in predictor_id_path_list:
         if model['gf_forest_id'] == predictor_id:
             # warp the predictor to be correct blocksize etc
@@ -100,6 +101,7 @@ def main():
                 dependent_task_list=[warp_task],
                 target_path_list=[gf_forest_cover_path],
                 task_name=f'gaussian filter {gf_forest_cover_path}')
+            gf_index = len(aligned_predictor_path_list)
             aligned_predictor_path_list.append(gf_forest_cover_path)
         else:
             aligned_predictor_path_list.append(warped_predictor_path)
@@ -112,37 +114,39 @@ def main():
     def _apply_model(*raster_nodata_array):
         n = len(raster_nodata_array)//2
         raster_array = raster_nodata_array[0:n]
-        nodata_array = raster_nodata_array[n:]
+        nodata_array = raster_nodata_array[n:2*n]
+        edge_override = raster_nodata_array[2*n]
+
         valid_mask = numpy.all(
             [~numpy.isclose(array, nodata) for array, nodata in
-             zip(raster_array, nodata_array)
-             if not isinstance(array, float)], axis=(0,))
+             zip(raster_array, nodata_array)], axis=(0,))
         result = numpy.full(valid_mask.shape, nodata)
         value_list = numpy.asarray([
-            numpy.full(valid_mask.shape, array)[valid_mask]
-            if isinstance(array, float) else array[valid_mask]
-            for array in raster_array]).transpose()
+            array[valid_mask] for array in raster_array])
+        if edge_override is not None:
+            value_list[gf_index] = numpy.array(
+                [edge_override]*value_list[0].size)
+        value_list.transpose()
         if value_list.shape[0] > 0:
             result[valid_mask] = train_regression_model.clip_to_range(
                 model['model'].predict(value_list), 10, 400)
         return result
 
-
     model_result_path = f'''full_forest_edge_result_{os.path.basename(os.path.splitext(
         args.forest_cover_path)[0])}.tif'''
     geoprocessing.raster_calculator(
-        [(path, 1) if path != gf_forest_cover_path else (1.0, 'raw') for path in aligned_predictor_path_list] +
+        [(path, 1) for path in aligned_predictor_path_list] +
         [(geoprocessing.get_raster_info(path)['nodata'][0], 'raw')
-         for path in aligned_predictor_path_list],
+         for path in aligned_predictor_path_list] + [(1.0, 'raw')],
         _apply_model, model_result_path,
         gdal.GDT_Float32, nodata)
 
     model_result_path = f'''no_forest_edge_result_{os.path.basename(os.path.splitext(
         args.forest_cover_path)[0])}.tif'''
     geoprocessing.raster_calculator(
-        [(path, 1) if path != gf_forest_cover_path else (0.0, 'raw') for path in aligned_predictor_path_list] +
+        [(path, 1) for path in aligned_predictor_path_list] +
         [(geoprocessing.get_raster_info(path)['nodata'][0], 'raw')
-         for path in aligned_predictor_path_list],
+         for path in aligned_predictor_path_list] + [(0.0, 'raw')],
         _apply_model, model_result_path,
         gdal.GDT_Float32, nodata)
 
@@ -151,7 +155,7 @@ def main():
     geoprocessing.raster_calculator(
         [(path, 1) for path in aligned_predictor_path_list] +
         [(geoprocessing.get_raster_info(path)['nodata'][0], 'raw')
-         for path in aligned_predictor_path_list],
+         for path in aligned_predictor_path_list] + [(None, 'raw')],
         _apply_model, model_result_path,
         gdal.GDT_Float32, nodata)
 
