4dd1f9764373949617b738c5c9835bf1acf7d263
Rich Sharp <rich@therealspring.org>
2022-07-14 13:23:00 -0700

    check to make sure that predictors are there

--- run_model.py
+++ run_model.py
@@ -2,6 +2,7 @@
 import argparse
 import pickle
 import logging
+import os
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -23,7 +24,7 @@ def main():
     parser.add_argument(
         'forest_cover_path', type=str, help='path to forest mask')
     parser.add_argument(
-        '--raster_directory', type=str,
+        '--predictor_raster_directory', type=str, default='',
         help='path to directory containing base data for model')
     parser.add_argument(
         '--prefix', type=str, help='string to add to output filename')
@@ -33,7 +34,20 @@ def main():
     with open(args.carbon_model_path, 'rb') as model_file:
         model = pickle.load(model_file).copy()
     LOGGER.info(f'ensure raster base data are present')
-    LOGGER.debug(f"{model['predictor_list']}")
+    predictor_id_to_path = {}
+    missing_predictor_list = []
+    for predictor_id in model['predictor_list']:
+        predictor_path = os.path.join(
+            args.predictor_raster_directory, f'{predictor_id}.tif')
+        if os.path.exists(predictor_path):
+            predictor_id_to_path[predictor_id] = predictor_path
+        else:
+            missing_predictor_list.append(
+                f'{predictor_id}: {predictor_path}')
+    if missing_predictor_list:
+        predictor_str = "\n".join(missing_predictor_list)
+        raise ValueError(
+            f'missing the following predictor rasters:\n{predictor_str}')
 
 
 if __name__ == '__main__':
