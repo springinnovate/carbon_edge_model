9fa68e93af95caecb025028248122ce8a8ffe5f6
Rich Sharp <rich@therealspring.org>
2022-07-14 15:48:49 -0700

    if not isinstance

--- run_model.py
+++ run_model.py
@@ -115,7 +115,8 @@ def main():
         nodata_array = raster_nodata_array[n:]
         valid_mask = numpy.all(
             [~numpy.isclose(array, nodata) for array, nodata in
-             zip(raster_array, nodata_array)], axis=(0,))
+             zip(raster_array, nodata_array)
+             if not isinstance(array, float)], axis=(0,))
         result = numpy.full(valid_mask.shape, nodata)
         value_list = numpy.asarray([
             array[valid_mask] for array in raster_array]).transpose()
@@ -129,7 +130,7 @@ def main():
     model_result_path = f'''full_forest_edge_result_{os.path.basename(os.path.splitext(
         args.forest_cover_path)[0])}.tif'''
     geoprocessing.raster_calculator(
-        [(path, 1) if path == gf_forest_cover_path else numpy.array([1.0]) for path in aligned_predictor_path_list] +
+        [(path, 1) if path == gf_forest_cover_path else (1.0, 'raw') for path in aligned_predictor_path_list] +
         [(geoprocessing.get_raster_info(path)['nodata'][0], 'raw')
          for path in aligned_predictor_path_list],
         _apply_model, model_result_path,
@@ -138,7 +139,7 @@ def main():
     model_result_path = f'''no_forest_edge_result_{os.path.basename(os.path.splitext(
         args.forest_cover_path)[0])}.tif'''
     geoprocessing.raster_calculator(
-        [(path, 1) if path == gf_forest_cover_path else numpy.array([0.0]) for path in aligned_predictor_path_list] +
+        [(path, 1) if path == gf_forest_cover_path else (0.0, 'raw') for path in aligned_predictor_path_list] +
         [(geoprocessing.get_raster_info(path)['nodata'][0], 'raw')
          for path in aligned_predictor_path_list],
         _apply_model, model_result_path,
