be5fd4e8c9f6e7df017f5e4d08d180b8f61f626c
Rich Sharp <rich@therealspring.org>
2022-08-22 13:05:59 -0700

    store result

--- carbon_edge_analysis.py
+++ carbon_edge_analysis.py
@@ -503,6 +503,7 @@ def main():
                 sum_task = task_graph.add_task(
                     func=sum_raster,
                     args=carbon_opt_step_path,
+                    store_result=True,
                     dependent_task_list=[optimization_carbon_task])
                 raster_sum_list.append(
                     (os.path.basename(carbon_opt_step_path), sum_task))
