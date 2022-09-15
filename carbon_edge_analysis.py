157e9b1e7de55c3e93ea2556d1d48667d0d73452
Rich Sharp <rich@therealspring.org>
2022-09-15 14:32:31 -0700

    keep going

--- carbon_edge_analysis.py
+++ carbon_edge_analysis.py
@@ -519,7 +519,6 @@ def main():
         task_name=f'regression marg value {REGRESSION_MARGINAL_VALUE_PATH}')
 
     regression_marginal_value_task.join()
-    return
 
     coarsen_forest_esa_mask_task = task_graph.add_task(
         func=ecoshard.convolve_layer,
