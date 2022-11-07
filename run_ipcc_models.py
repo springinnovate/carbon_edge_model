129c3c0679591209d67d529c4269192755c6c532
Rich Sharp <richpsharp@gmail.com>
2022-11-07 12:42:06 -0800

    make sure to uncoarsen

--- run_ipcc_models.py
+++ run_ipcc_models.py
@@ -6,10 +6,13 @@ import multiprocessing
 
 from run_model import regression_carbon_model
 from run_model import GLOBAL_BOUNDING_BOX_TUPLE
+from run_model import ECKERT_PIXEL_SIZE
+from run_model import WORLD_ECKERT_IV_WKT
+from run_model import ZSTD_CREATION_TUPLE
 from ecoshard import taskgraph
 from ecoshard import geoprocessing
 
-BASE_FOREST_MASK_PATH = 'forest_mask_esa.tif'
+BASE_FOREST_MASK_PATH = './output_global/forest_mask_esa.tif'
 CARBON_MODEL_PATH = './models/hansen_model_2022_07_14.dat'
 PREDICTOR_RASTER_DIR = './processed_rasters'
 PRE_WARP_DIR = os.path.join(PREDICTOR_RASTER_DIR, f'pre_warped_{GLOBAL_BOUNDING_BOX_TUPLE[0]}')
@@ -51,20 +54,51 @@ def main():
     """Entry point."""
     task_graph = taskgraph.TaskGraph('./output_global/ipcc_optimization', multiprocessing.cpu_count(), 15)
     search_path = './output_global/ipcc_optimization/ipcccoarsened_marginal_value_ipcc_mask_*.tif'
-    for new_forest_mask_path in glob.glob(search_path):
-        area_substring = os.path.splitext((new_forest_mask_path.split('_')[-1]))[0]
-        combined_forest_mask_path = f'./output_global/ipcc_optimization/ipcc_total_forest_mask_{area_substring}.tif'
-        add_masks(new_forest_mask_path, BASE_FOREST_MASK_PATH, combined_forest_mask_path)
+    full_forest_mask_path_list = []
+    for coarse_new_forest_mask_path in glob.glob(search_path):
+        area_substring = os.path.splitext((coarse_new_forest_mask_path.split('_')[-1]))[0]
 
+        carbon_opt_forest_step_path = (
+            '%s_uncoarsened_forest_mask%s' % os.path.splitext(coarse_new_forest_mask_path))
+        uncoarsen_forest_mask_task = task_graph.add_task(
+            func=geoprocessing.warp_raster,
+            args=(coarse_new_forest_mask_path, ECKERT_PIXEL_SIZE,
+                  carbon_opt_forest_step_path, 'near'),
+            kwargs={
+                'target_bb': GLOBAL_BOUNDING_BOX_TUPLE[1],
+                'target_projection_wkt': WORLD_ECKERT_IV_WKT,
+                'n_threads': multiprocessing.cpu_count(),
+                'working_dir': PRE_WARP_DIR,
+                'raster_driver_creation_tuple': ZSTD_CREATION_TUPLE
+            },
+            target_path_list=[carbon_opt_forest_step_path],
+            task_name=f'uncoarsen {carbon_opt_forest_step_path}')
+
+        combined_forest_mask_path = f'./output_global/ipcc_optimization/ipcc_total_forest_mask_{area_substring}.tif'
+        _ = task_graph.add_task(
+            func=add_masks,
+            args=(carbon_opt_forest_step_path, BASE_FOREST_MASK_PATH,
+                  combined_forest_mask_path),
+            target_path_list=[combined_forest_mask_path],
+            dependent_task_list=[uncoarsen_forest_mask_task],
+            task_name=f'add {carbon_opt_forest_step_path} to ESA base')
+        full_forest_mask_path_list.append(carbon_opt_forest_step_path)
+
+    task_graph.join()
+    for full_forest_mask_path in full_forest_mask_path_list:
         modeled_carbon_path = f'./output_global/ipcc_optimization/ipcc_carbon_modeled_by_regression_{area_substring}.tif'
         LOGGER.debug(f'calculating carbon for {modeled_carbon_path}')
+
         regression_carbon_model(
             CARBON_MODEL_PATH, GLOBAL_BOUNDING_BOX_TUPLE,
-            combined_forest_mask_path, PREDICTOR_RASTER_DIR,
+            full_forest_mask_path, PREDICTOR_RASTER_DIR,
             pre_warp_dir=PRE_WARP_DIR,
             target_result_path=modeled_carbon_path,
             external_task_graph=task_graph,
             clean_workspace=False)
+
+    task_graph.join()
+    task_graph.close()
 
 
 if __name__ == '__main__':
